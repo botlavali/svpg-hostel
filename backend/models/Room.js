0abfe1e086f88ee3721d285a4c916aac651a2a32
botlavali <botlavali6420@gmail.com>
2025-11-23 00:58:57 -0800

    mohansvpg

--- backend/models/Room.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const roomSchema = new mongoose.Schema({
-  roomNumber: { type: String, required: true },
-  beds: [{ number: Number, isBooked: { type: Boolean, default: false } }],
-});
-
-module.exports = mongoose.model("Room", roomSchema);
