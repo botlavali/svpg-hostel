4f242033190448f2ac86bb45012ce15c058b91c5
botlavali <botlavali6420@gmail.com>
2025-11-23 10:02:01 -0800

    Update api.js

--- admin-panel/src/api.js
+++ admin-panel/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // adjust backend URL if different
+  baseURL: "https://mohansvpg-backend.onrender.com", // Render backend URL
+  withCredentials: true,
 });
 
 export default api;
