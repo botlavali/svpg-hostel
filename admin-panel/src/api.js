d65ddb123e0986dc516e5c2e397e11d1422a6091
botlavali <botlavali6420@gmail.com>
2025-11-30 21:58:36 +0530

    Update api.js

--- admin-panel/src/api.js
+++ admin-panel/src/api.js
@@ -1,8 +1,4 @@
-// frontend/src/api.js
+// admin-panel/src/api.js or admin-panel/services/api.js
 import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://svpg-hostel.onrender.com", 
-});
-
+const api = axios.create({ baseURL: "https://svpg-hostel.onrender.com" });
 export default api;
