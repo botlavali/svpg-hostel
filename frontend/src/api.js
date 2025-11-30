58d4d5ce4389f38dca686003e6567ee4380397f1
botlavali <botlavali6420@gmail.com>
2025-11-30 16:51:24 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-<<<<<<< HEAD
-  baseURL: "https://svpg-backend.onrender.com",   // NO /api
-=======
-  baseURL: "https://37ptgzfs-5000.inc1.devtunnels.ms",   // NO /api
->>>>>>> 4280d279 (Updated API URL for live backend)
+  baseURL: "https://svpg-backend.onrender.com",
 });
 
 export default api;
+
