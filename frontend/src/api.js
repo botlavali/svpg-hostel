0778cf9ee129e6371a2890bf2d7dd069525b14f1
botlavali <botlavali6420@gmail.com>
2025-11-30 16:36:03 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",   // NO /api
+  baseURL: "https://svpg-backend.onrender.com",   // NO /api
 });
 
 export default api;
