a7f782f2240fff0848f5c49a0bd7472bd382e79b
botlavali <botlavali6420@gmail.com>
2025-11-30 16:36:21 +0530

    git push Ctrl + O   (save)Updated API URL for live backend Ctrl + X   (exit) code frontend/src/api.js
    :wq
    :w# Last command done (1 command done):
    :wq

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
+<<<<<<< HEAD
   baseURL: "https://svpg-backend.onrender.com",   // NO /api
+=======
+  baseURL: "https://37ptgzfs-5000.inc1.devtunnels.ms",   // NO /api
+>>>>>>> 4280d279 (Updated API URL for live backend)
 });
 
 export default api;
