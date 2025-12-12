8fcb4ae3b739056cc8b004c679a5cd5af4b25971
botlavali <botlavali6420@gmail.com>
2025-12-12 11:15:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ const app = express();
 const allowedOrigins = [
   "https://svpghostel.vercel.app",   // main frontend (users)
   "https://svpg-hostel.vercel.app",
-  "https://svpg-hostel-sxi8.vercel.app", // admin frontend
+  "https://svpg-hostel-sxi8.vercel.app",
+  "https://your-frontend-render-url.onrender.com",// admin frontend
   "http://localhost:3000",
   "http://localhost:3001"
 ];
